b33eed9a6494d49785e92c71741bb79592c12610
Rohan Jansen <rohan@flatcircle.io>
2017-10-05 15:56:56 +0200

    move main functions into protocol

--- FlatNetworkClient/NetworkProtocols.swift
+++ FlatNetworkClient/NetworkProtocols.swift
@@ -12,7 +12,13 @@ typealias NetworkResult = (Data?, Error?) -> Void
 
 protocol NetworkConnectable {
     var tasks: [String: URLSessionTask] { get set }
-    var session: URLSessionInjectable { get set }}
+    var session: URLSessionInjectable { get set }
+    
+    func get<A>(_ endPoint: GetEndpointCreator, type: A.Type?, completion: @escaping (A?, Error?) -> Void) where A: JsonCreatable
+    func post<A>(_ endPoint: PostEndpointCreator, type: A.Type?, completion: @escaping (A?, Error?) -> Void) where A: JsonCreatable
+    func delete<A>(_ endPoint: DeleteEndpointCreator, type: A.Type?, completion: @escaping (A?, Error?) -> Void) where A: JsonCreatable
+    func put<A>(_ endPoint: PutEndpointCreator, type: A.Type?, completion: @escaping (A?, Error?) -> Void) where A: JsonCreatable
+}
 
 protocol URLSessionInjectable {
     func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) -> URLSessionDataTask
