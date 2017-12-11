419d7fbade7c6d2d2dde6007b53d98d3314a9080
Rohan Jansen <rohan@flatcircle.io>
2017-12-11 09:38:47 +0200

    add decrypt functionality

--- FlatNetworkClient/JSONProtocols.swift
+++ FlatNetworkClient/JSONProtocols.swift
@@ -14,10 +14,16 @@ public protocol JsonCreatable {
     static func createFromJSON(_ json: A?) -> T?
 }
 
+extension JsonCreatable {
+    static func decrypt(_ data: Data?) -> Data? {
+        return data
+    }
+}
+
 //TODO: perhaps look at adding a swift 4 implementation and see if it works with solutions that are swift 3 and down comaptible
 extension JsonCreatable {
     public static func createFromData(_ data: Data?) -> T? {
-        if let data = data,
+        if let data = Self.decrypt(data),
             let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? A {
             return self.createFromJSON(json)
         } else {
